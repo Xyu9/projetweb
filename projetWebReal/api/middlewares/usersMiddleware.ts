c8721b72ee547b53aa47b504f636938cf70326c5
Pascal Chicoine <1836442@cegep-lanaudiere.qc.ca>
2024-01-30 12:57:25 -0500

    ajout

--- projetWebReal/api/middlewares/usersMiddleware.ts
+++ projetWebReal/api/middlewares/usersMiddleware.ts
@@ -1,19 +1,44 @@
-import {checkSchema, validationResult} from "express-validator";
-import {ExistsOptions} from "express-validator/src/chain";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../models/users";
+import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/users";
 
-export class userMiddleware {
+export const usersMiddleware = {
+  accountExist: async (req: Request, res: Response, next: NextFunction) => {
+    const { username } = req.body;
 
-  static isAdmin(req: Request, res: Response, next: NextFunction) {
-    let isAdmin;
-    if (!isAdmin) {
-      res.sendStatus(403);
+    try {
+      const user: any | null = await User.findOne({ username });
+
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+
+      // Attach the user to the request object for future middleware/routes
+      //req.user = user;
+
+      next(); // Continue to the next middleware or route
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
     }
-    else {
-      next();
+  },
+  accountNotExist: async (req: Request, res: Response, next: NextFunction) => {
+    const { username } = req.body;
+
+    try {
+      const user: any | null = await User.findOne({ username });
+
+      if (user) {
+        return res.status(401).json({ message: 'Compte Existant' });
+      }
+
+      // Attach the user to the request object for future middleware/routes
+      //req.user = user;
+
+      next(); // Continue to the next middleware or route
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
     }
-  }
-
-}
-
+  },
+};
--- projetWebReal/api/routes/userRouter.ts
+++ projetWebReal/api/routes/userRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import {userController} from '../controllers/userController';
+import { usersMiddleware } from "../middlewares/usersMiddleware";
 
 // Registration route
-router.post('/register', userController.register);
-router.post('/login', userController.login);
+router.post('/register',usersMiddleware.accountNotExist, userController.register);
+router.post('/login',usersMiddleware.accountExist ,userController.login);
 
 //module.exports = router;
 
